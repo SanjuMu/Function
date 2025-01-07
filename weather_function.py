232819c7f444cf66a3268b3d817f9ce8b8a4d27b
SanjuMu <sanjusri.munigala@gmail.com>
2025-01-06 19:27:19 -0500

    weather_function

---
+++ weather_function.py
@@ -0,0 +1,11 @@
+def weather_function():
+    print("The weather is pleasent in:", spring)
+    print("The weather is same in: ", autumn )
+
+
+spring = "autumn"
+autumn = "spring"
+
+weather_function()
+
+
